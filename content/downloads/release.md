d99374e98c265f6b1596100db57dc8607f64e267
Zac Bergquist <zbergquist99@gmail.com>
2016-07-27 11:22:26 -0600

    [#126886517] add link to latest github releases for omg cli

--- content/downloads/release.md
+++ content/downloads/release.md
@@ -1,14 +1,14 @@
 +++
 title = "Downloads"
-description = "CFOps downloads"
+description = "enaml downloads"
 date = "2016-01-14"
 bucket = "release"
-file = "cfops"
+file = "enaml"
 +++
 
-# CFOps Downloads
+# OMG CLI Downloads
 
-Below are the available downloads for the latest version of cfops. Please
-download the proper package for your operating system. You can also download
-older versions of cfops from the
-[GitHub releases page](https://github.com/pivotalservices/cfops/releases).
+Below are the available downloads for the latest version of the OMG CLI.
+Please download the proper package for your operating system.
+You can also download older versions of OMG from the
+[GitHub releases page](https://github.com/enaml-ops/omg-cli/releases).
--- themes/cf/static/js/releases.js
+++ themes/cf/static/js/releases.js
@@ -1,66 +1,70 @@
-var BUCKET_URL = 'https://pivotal-cfops.s3.amazonaws.com';
+var OMG_GITHUB_REPO = "enaml-ops/omg-cli"
 
 jQuery(function($) {
-  setDownloadLinksToLatestVersion();
+  //setDownloadLinksToLatestVersion();
+  getLatestGithubRelease(OMG_GITHUB_REPO);
 });
 
-function setDownloadLinksToLatestVersion() {
-  $.get(BUCKET_URL)
-    .done(function(data) {
-      var xml = $(data);
-      var versions = getReleasedVersionsFromS3Data(xml);
-      if (versions.length > 0) {
-        versions.sort(semverCompare);
-        var latestVersion = versions[versions.length - 1];
-        $("#version").text("Latest Release v" + latestVersion);
-        $(".fa-apple a").attr("href", downloadUrl(latestVersion, "osx"));
-        $(".fa-linux a").attr("href", downloadUrl(latestVersion, "linux64"));
-        $(".fa-windows a").attr("href", downloadUrl(latestVersion, "win64"));
+// A slightly modified version of:
+// https://stackoverflow.com/questions/24987542/is-there-a-link-to-github-for-downloading-a-file-in-the-latest-release-of-a-repo
+function getLatestGithubRelease(repo) {
+    var url = "https://api.github.com/repos/" + repo + "/releases/latest"
+    $.getJSON(url).done(function (release) {
+
+      // update the version in the header
+      $("#version").text("Latest Release " + release.tag_name);
+
+      // update individual download links
+      for (var j = 0; j < release.assets.length; j++) {
+        var asset = release.assets[j];
+        console.log("found release: " + asset.name)
+
+        // TODO: for now this is kind of brute forced to search for omg only
+        if (!asset.name.startsWith("omg-")) {
+          continue;
+        }
+        var parts = asset.name.split('-');
+        var platform = parts.pop(); // 'osx', 'linux', etc.
+        var downloadCount = asset.download_count;
+
+        // compute time since last update
+        var oneHour = 60 * 60 * 1000
+        var oneDay = 24 * oneHour
+        var dateDiff = new Date() - new Date(asset.updated_at)
+        var timeAgo;
+        if (dateDiff < oneDay) {
+          timeAgo = (dateDiff / oneHour).toFixed(0) + " hours ago";
+        } else {
+          timeAgo = (dateDiff / oneDay).toFixed(0) + " days ago";
+        }
+
+        var sel
+        switch (platform) {
+          case "osx":
+            sel = ".fa-apple ";
+            break;
+          case "linux":
+            sel = ".fa-linux ";
+            break;
+          // TODO: we aren't currently publishing windows releases...
+          default:
+            continue;
+        }
+
+        // update download link
+        $(sel + "a").attr("href", asset.browser_download_url)
+
+        // add release info
+        var releaseInfo = "updated " + timeAgo + ", " + plural(downloadCount);
+        $(sel + "span").text(releaseInfo);
+        $(sel + "span").fadeIn("slow");
       }
     })
-    .fail(function(error) {
-      console.error(error);
-    });
 }
 
-// Gets all version numbers under the S3 /release/osx folder
-function getReleasedVersionsFromS3Data(xml) {
-  var bucket = $("#bucket").text();
-  var files = $.map(xml.find('Contents'), function(item) {
-    item = $(item);
-    file = item.find('Key').text();
-	if (file.indexOf(bucket) > -1) {
-		reg = /osx\/v(\d+\.\d+\.\d+)/i
-		m = file.match(reg);
-		if (m) {
-      		return m[1];
-    	}
-	}
-  });
-  return files;
-}
-
-function downloadUrl(latestVersion, platform) {
-  var bucket = $("#bucket").text();
-  var fileName = $("#fileName").text();
-  var url = BUCKET_URL + "/" + bucket + "/" + platform + "/v" + latestVersion + "/" + fileName
-  if (platform === "win64") {
-    url = url + ".exe";
+function plural(count) {
+  if (count == 1) {
+    return count.toLocaleString() + " download"
   }
-  console.info(url);
-  return url;
+  return count.toLocaleString() + " downloads"
 }
-
-function semverCompare(a, b) {
-  var pa = a.split('.');
-  var pb = b.split('.');
-  for (var i = 0; i < 3; i++) {
-    var na = Number(pa[i]);
-    var nb = Number(pb[i]);
-    if (na > nb) return 1;
-    if (nb > na) return -1;
-    if (!isNaN(na) && isNaN(nb)) return 1;
-    if (isNaN(na) && !isNaN(nb)) return -1;
-  }
-  return 0;
-};
